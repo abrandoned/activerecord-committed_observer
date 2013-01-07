794604992a4bf29ae71b3b37f1bb00fc8a3f2e52
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-01-07 13:42:46 -0700

    add after_commit_on_commit and warning message when after_commit is defined

--- lib/activerecord-committed_observer.rb
+++ lib/activerecord-committed_observer.rb
@@ -3,7 +3,30 @@ require "activerecord-committed_observer/version"
 module ActiveRecord
   module CommittedObserver
 
+    def self.included(klass)
+      klass.class_eval do 
+        def self.method_added(method_name)
+          if method_name == :after_commit
+            if Rails.env != 'production'
+              warn <<-SUPER_WARN
+                If you are using ActiveRecord::CommittedObserver and defined an :after_commit
+                method make sure you call super to have the after_commit_on* series of methods
+                called.  
+
+                Instead of :after_commit you can also define :after_commit_on_commit for the
+                same functionality without the 'warn' message.
+
+                ^ after_commit_on_commit will be called before any other after_commit_on_* methods
+              SUPER_WARN
+            end
+          end
+        end
+      end
+    end
+
     def after_commit(model)
+      after_commit_on_commit(model) if respond_to?(:after_commit_on_commit)
+
       case
       when model.__send__(:transaction_include_action?, :create) then
         after_commit_on_create(model) if respond_to?(:after_commit_on_create)
